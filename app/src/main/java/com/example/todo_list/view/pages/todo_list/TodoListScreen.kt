7191f6ef556fc4a014255546e3190e00a512dec2
yura <yura.zb.ua@gmail.com>
2024-02-22 12:56:56 +0200

    fix search task in database fix checkbox in UI

--- app/src/main/java/com/example/todo_list/view/pages/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/example/todo_list/view/pages/todo_list/TodoListScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -32,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.todo_list.R
 import com.example.todo_list.view.model.TaskUi
@@ -118,7 +120,9 @@ private fun TodoListScreenContent(
     uiState: TodoListUiState?,
     uiEvent: (TodoListUiEvent) -> Unit
 ) {
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier
+    ) {
         Text(
             modifier = Modifier
                 .padding(top = 8.dp),
@@ -146,7 +150,6 @@ private fun TodoListItem(
     task: TaskUi,
     uiEvent: (TodoListUiEvent) -> Unit,
 ) {
-    val checkedState = remember { mutableStateOf(task.isDone) }
     val isShowDeleteConfirmDialog = remember { mutableStateOf(false) }
 
     Row(
@@ -160,71 +163,71 @@ private fun TodoListItem(
             )
             .clickable {
                 //checkedState.value = !checkedState.value
-                uiEvent(TodoListUiEvent.OnCheckClick(task))
-            }
+            },
     ) {
+        // Checkbox
         Column(
-            modifier = Modifier.fillMaxHeight(),
+            modifier = Modifier
+                .width(40.dp)
+                .fillMaxHeight(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Checkbox(
                 checked = task.isDone,
-                onCheckedChange = { checkedState.value = it },
+                onCheckedChange = {
+                    uiEvent(TodoListUiEvent.OnCheckClick(task))
+                },
                 colors = CheckboxDefaults.colors(
                     uncheckedColor = Yellow100,
                     checkedColor = Yellow100
                 )
             )
         }
-        Row(
+        // Content
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(end = 8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+                .weight(1f)
+                .fillMaxHeight()
+                .padding(top = 8.dp, bottom = 8.dp),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(top = 8.dp, bottom = 8.dp),
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = task.time,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-
-                Text(
-                    text = task.description,
-                    style = MaterialTheme.typography.bodyLarge,
-                    textDecoration = if (checkedState.value) TextDecoration.LineThrough
-                    else TextDecoration.None
-                )
+            Text(
+                text = task.time,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = task.description,
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                textDecoration = if (task.isDone) TextDecoration.LineThrough
+                else TextDecoration.None
+            )
+        }
+        // Icons
+        Column(
+            modifier = Modifier
+                .width(40.dp)
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            EditIconButton {
+                uiEvent(TodoListUiEvent.OnEditClick(task.taskId))
             }
-            Column(
-                modifier = Modifier
-                    .fillMaxHeight(),
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.End
-            ) {
-                EditIconButton {
-                    uiEvent(TodoListUiEvent.OnEditClick(task.taskId))
-                }
-                DeleteIconButton {
-                    isShowDeleteConfirmDialog.value = true
-                }
+            DeleteIconButton {
+                isShowDeleteConfirmDialog.value = true
+            }
 
-                if (isShowDeleteConfirmDialog.value) {
-                    DeleteConfirmDialog(
-                        onDismissRequest = { isShowDeleteConfirmDialog.value = false },
-                        onConfirmation = {
-                            uiEvent(TodoListUiEvent.OnDeleteClick(task.taskId))
-                            isShowDeleteConfirmDialog.value = false
-                        },
-                        dialogTitle = task.description,
-                    )
-                }
+            if (isShowDeleteConfirmDialog.value) {
+                DeleteConfirmDialog(
+                    onDismissRequest = { isShowDeleteConfirmDialog.value = false },
+                    onConfirmation = {
+                        uiEvent(TodoListUiEvent.OnDeleteClick(task.taskId))
+                        isShowDeleteConfirmDialog.value = false
+                    },
+                    dialogTitle = task.description,
+                )
             }
         }
     }
